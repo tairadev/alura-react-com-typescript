e44fc286d50b9962a92974287e370933c624245a
Guilherme Taira <guilherme.taira@onr.org.br>
2025-08-05 21:04:39 -0300

    componente link

--- luz-e-cena/src/App.tsx
+++ luz-e-cena/src/App.tsx
@@ -1,8 +1,14 @@
 import Banner from "./components/Banner"
+import Link from "./components/Link"
 
 function App() {
   return (
-    <Banner src="./banner.jpg" alt="Banner" />
+    <>
+      <Banner src="./banner.jpg" alt="Banner" />
+      <Link href="/" target="_blank">
+        Link
+      </Link>
+    </>
   )
 }
 
