883275ebffb3cd8925e72b4f410762c33d8b64ff
Guilherme Taira <guilherme.taira@onr.org.br>
2025-08-05 23:41:37 -0300

    fazendo requisições dos dados

--- luz-e-cena/src/components/MovieSection/index.tsx
+++ luz-e-cena/src/components/MovieSection/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './MovieSection.module.css'
 import Fieldset from '../Fieldset'
 import InputText from '../InputText'
 import Button from '../Button'
 import { FaSearch } from 'react-icons/fa'
 import MovieList from '../MovieList'
+import type { Movie } from '../../types'
+import { getMovies } from '../../services'
 
 const MovieSection = () => {
+  const [movies, setMovies] = useState<Movie[]>([])
+
+  const fetchMovies = async () => {
+    try {
+      const resMovies = await getMovies()
+      setMovies(resMovies);
+    } catch (err) {
+      console.error("Erro ao buscar os filmes" + err)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  })
+
   return (
     <main>
       <section className={styles.container}>
