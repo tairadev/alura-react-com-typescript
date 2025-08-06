8b6d4b60cf6ed9206d96e613e3eef3c990dd5622
Guilherme Taira <guilherme.taira@onr.org.br>
2025-08-05 21:43:08 -0300

    criando o selectoroption

---
+++ luz-e-cena/src/components/Header/components/HeaderFormFilters.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import SelectorGroup from '../../Selector/SelectorGroup'
+import { FaLocationDot } from 'react-icons/fa6'
+import SelectorOption from '../../Selector/SelectorOption'
+
+const cities = [
+  { id: 1, name: 'São Paulo' },
+  { id: 2, name: 'Rio de Janeiro' },
+  { id: 3, name: 'Belo Horizonte' },
+  { id: 4, name: 'Curitiba' },
+  { id: 5, name: 'Porto Alegre' },
+  { id: 6, name: 'Salvador' },
+]
+
+const HeaderFormFilters = () => {
+  return (
+    <form>
+      <SelectorGroup id="cidade" icon={<FaLocationDot />}>
+        <SelectorOption value="" label="Selecione uma cidade" />
+        { cities.map(cidade => (
+          <SelectorOption key={cidade.id} value={cidade.name} label={cidade.name} />
+        )) }
+      </SelectorGroup>
+    </form>
+  )
+}
+
+export default HeaderFormFilters
