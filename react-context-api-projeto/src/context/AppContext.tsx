7d5458c079108f83ad76bc7d2fb5db810b1f2398
Guilherme Taira <guilherme.taira@onr.org.br>
2025-08-07 12:19:56 -0300

    Manipulando o estado de usuário

--- react-context-api-projeto/src/context/AppContext.tsx
+++ react-context-api-projeto/src/context/AppContext.tsx
@@ -1,15 +1,43 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { IUsuario } from '../types'
+import { obterUsuario } from '../services';
 
 interface AppContextType {
   usuario: IUsuario | null;
+  criarUsuario: (usuario: Omit<IUsuario, "id">) => Promise<void>
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined)
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const [usuario, setUsuario] = useState<IUsuario | null>(null)
+
+  const carregaDadosUsuario = async () => {
+    try {
+      const usuarios = await obterUsuario();
+      if (usuarios.length > 0) {
+        setUsuario(usuarios[0])
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    carregaDadosUsuario()
+  }, [])
+
+  const criarUsuario = async (usuario: Omit<IUsuario, "id">) => {
+    try {
+      const novoUsuario = await criarUsuario(usuario);
+      setUsuario(novoUsuario)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <AppContext.Provider value={{}}>
+    <AppContext.Provider value={{ usuario, criarUsuario }}>
       {children}
     </AppContext.Provider>
   )
